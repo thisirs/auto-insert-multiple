bb4a2b540643770acf6207ffaa62880b99010011
thisirs <thisirs@gmail.com>
2012-03-05 17:24:10 +0100

    update header file

--- auto-insert.el
+++ auto-insert.el
@@ -1,27 +1,25 @@
-;;; autoinsert.el --- automatic mode-dependent insertion of text into new files
+;;; auto-insert.el --- automatic mode-dependent insertion of text into new files
 
 ;; Copyright (C) 1985-1987, 1994-1995, 1998, 2000-2012
 ;;   Free Software Foundation, Inc.
 
 ;; Author: Charlie Martin <crm@cs.duke.edu>
 ;; Adapted-By: Daniel Pfeiffer <occitan@esperanto.org>
+;; Adapted-By: Sylvain Rousseau <thisirs@gmail.com>
 ;; Keywords: convenience
-;; Maintainer: FSF
 
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software: you can redistribute it and/or modify
+;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
